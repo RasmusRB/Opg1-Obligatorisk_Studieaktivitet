4fee3af234339355e29f605e1c3833762953c96c
RasmusRB <57805908+RasmusRB@users.noreply.github.com>
2020-09-07 09:55:10 +0200

    Add project files.

---
+++ CykelLib/Cykel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CykelLib
+{
+    public class Cykel
+    {
+        private int _id;
+        private string _color;
+        private int _price;
+        private int _gear;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Color
+        {
+            get { return _color; }
+            set
+            {
+
+            }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+
+            }
+        }
+
+        public int Gear
+        {
+            get { return _gear; }
+            set
+            {
+
+            }
+        }
+    }
+}
